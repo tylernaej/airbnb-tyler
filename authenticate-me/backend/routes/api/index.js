3c9853df324a1713ba096631cfe0fa64c474b610
tyler <tylernaej@gmail.com>
2022-07-29 13:00:55 -0400

    Add User authentication Express middleware

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,30 +1,51 @@
 // backend/routes/api/index.js
 const router = require('express').Router();         //const express = require('express');
                                                     //const router = express.Router();
+const { restoreUser } = require('../../utils/auth.js');
+// backend/routes/api/index.js
+// ...
+router.use(restoreUser);
 
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// ...
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body});
+//   });
+
+// ...
 // backend/routes/api/index.js
 // ...
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body});
-  });
+// GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
 
-// ...
-// backend/routes/api/index.js
 // ...
 
 // GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      }
-    });
-  setTokenCookie(res, user);
-  return res.json({ user });
-});
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// });
 
 // ...
 
