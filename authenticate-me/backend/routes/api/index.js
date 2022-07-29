bba51930abda755d3e67135aa513c207eb978856
tyler <tylernaej@gmail.com>
2022-07-29 13:14:48 -0400

    Add User login backend endpoint

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,52 +1,20 @@
 // backend/routes/api/index.js
-const router = require('express').Router();         //const express = require('express');
-                                                    //const router = express.Router();
-const { restoreUser } = require('../../utils/auth.js');
-// backend/routes/api/index.js
-// ...
+const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
+
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
-// router.get(
-//   '/restore-user',
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
+router.use('/session', sessionRouter);
 
-// ...
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body});
-//   });
+router.use('/users', usersRouter);
 
-// ...
-// backend/routes/api/index.js
-// ...
-
-// GET /api/require-auth
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-
-// ...
-
-// GET /api/set-token-cookie
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// router.get('/set-token-cookie', async (_req, res) => {
-//   const user = await User.findOne({
-//       where: {
-//         username: 'Demo-lition'
-//       }
-//     });
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// });
-
-// ...
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
