853dc1f63dacde933779c66dc0c13632f9478f8a
tyler <tylernaej@gmail.com>
2022-07-31 19:44:51 -0400

    added frontend folder

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -29,64 +29,64 @@ app.use(
   })
   );
 
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-    );
-    // backend/app.js
-    const routes = require('./routes');
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+  );
+  // backend/app.js
+  const routes = require('./routes');
 
-    // ...
+  // ...
 
-    app.use(routes); // Connect all the routes
+  app.use(routes); // Connect all the routes
 
 
-    // backend/app.js
-    // ...
+  // backend/app.js
+  // ...
 
-    app.use((_req, _res, next) => {
-      const err = new Error("The requested resource couldn't be found.");
-      err.title = "Resource Not Found";
-      err.errors = ["The requested resource couldn't be found."];
-      err.status = 404;
-      next(err);
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+
+  // backend/app.js
+  // ...
+  const { ValidationError } = require('sequelize');
+
+  // ...
+
+  // Process sequelize errors
+  app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+  });
+
+  // backend/app.js
+  // ...
+  // Error formatter
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
     });
-
-
-    // backend/app.js
-    // ...
-    const { ValidationError } = require('sequelize');
-
-    // ...
-
-    // Process sequelize errors
-    app.use((err, _req, _res, next) => {
-      // check if error is a Sequelize error:
-      if (err instanceof ValidationError) {
-        err.errors = err.errors.map((e) => e.message);
-        err.title = 'Validation error';
-      }
-      next(err);
-    });
-
-    // backend/app.js
-    // ...
-    // Error formatter
-    app.use((err, _req, res, _next) => {
-      res.status(err.status || 500);
-      console.error(err);
-      res.json({
-        title: err.title || 'Server Error',
-        message: err.message,
-        errors: err.errors,
-        stack: isProduction ? null : err.stack
-      });
-    });
+  });
 
     module.exports = app;
