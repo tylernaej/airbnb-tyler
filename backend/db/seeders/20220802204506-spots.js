db4649654816bf6080dfd0010267e2ff90dc107e
tyler <tylernaej@gmail.com>
2022-08-02 17:17:44 -0400

    added booking seed data

--- backend/db/seeders/20220802204506-spots.js
+++ backend/db/seeders/20220802204506-spots.js
@@ -62,11 +62,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Spots', null, {});
   }
 };
