5304409b19e5d54666437d7359611cb0e1107b19
tyler <tylernaej@gmail.com>
2022-08-03 09:43:21 -0400

    added getSpot and getSpot:id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,52 @@
 const express = require('express');
 const router = express.Router();
 
-const {Booking, Image, Review, Spot, User} = require('../../db/models');
+const {Booking, Image, Review, Spot, User, sequelize} = require('../../db/models');
+
+router.get('/', async(req, res) => {
+    const spots = await Spot.findAll({
+        attributes: {
+            include: [
+                [sequelize.fn("AVG", sequelize.col("Reviews.stars")),
+                "avgRating"],
+                [sequelize.literal("Images.url"), "previewImage"]
+            ]
+        },
+        include: [
+            {
+                model: Review,
+                attributes: []
+            },
+            {
+                model: Image,
+                where: {
+                    previewImage: true
+                },
+                attributes: []
+            }
+        ]
+    })
+    res.status(200)
+    res.json(spots)
+})
 
 router.get('/:spotId', async(req, res) => {
     const spot = await Spot.findByPk(req.params.spotId, {
-
+        attributes: {
+            include: [[sequelize.fn("AVG", sequelize.col("Reviews.stars")),
+            "avgStarRating"],
+            [sequelize.fn("COUNT", sequelize.col("Reviews.stars")),
+            "numReviews"
+        ]]},
+        include: {
+            model: Review,
+            include: {
+                model: User,
+                include: {
+                    model: Image, 
+                },
+            }
+        }
     })
     res.json(spot)
 })
