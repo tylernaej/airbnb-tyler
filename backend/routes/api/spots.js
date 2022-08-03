afaebe050e0483fc8fc733521163d80f884c20e6
tyler <tylernaej@gmail.com>
2022-08-03 16:15:18 -0400

    added GET all spots, get current spot, getspot:id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,33 +1,39 @@
 const express = require('express');
 const router = express.Router();
-
+const { restoreUser } = require('../../utils/auth');
 const {Booking, Image, Review, Spot, User, sequelize} = require('../../db/models');
 
 router.get('/', async(req, res) => {
+    let response = {}
     const spots = await Spot.findAll({
+        // raw: true,
         attributes: {
-            include: [
-                [sequelize.fn("AVG", sequelize.col("Reviews.stars")),
-                "avgRating"],
-                [sequelize.literal("Images.url"), "previewImage"]
-            ]
-        },
-        include: [
-            {
-                model: Review,
-                attributes: []
-            },
-            {
-                model: Image,
-                where: {
-                    previewImage: true
-                },
-                attributes: []
-            }
-        ]
-    })
+            include: [[sequelize.fn("AVG", sequelize.col("Reviews.stars")),
+            "avgStarRating"],
+        ]},
+        include: 
+        [
+            {model: Review, attributes: []}, 
+            {model: Image, attributes: ['url']},
+        ],
+        group: ["Spot.id"]
+    });
+    response.spots = spots
     res.status(200)
-    res.json(spots)
+    res.json(response)
+})
+
+router.get('/current',
+    restoreUser,
+    async(req, res) => {
+        const { user } = req;
+        console.log(user.dataValues.id)
+        const userSpots = await Spot.findAll({
+            where: {
+                ownerId: user.dataValues.id
+            }
+        })
+    res.json(userSpots)
 })
 
 router.get('/:spotId', async(req, res) => {
@@ -38,15 +44,19 @@ router.get('/:spotId', async(req, res) => {
             [sequelize.fn("COUNT", sequelize.col("Reviews.stars")),
             "numReviews"
         ]]},
-        include: {
-            model: Review,
-            include: {
-                model: User,
+        include: 
+        [
+            {
+                model: Review,                
+                group: ['Review.id'],
                 include: {
                     model: Image, 
                 },
+            },
+            {
+                model: User
             }
-        }
+        ],
     })
     res.json(spot)
 })
