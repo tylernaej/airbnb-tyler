76e8976416735dc9018e2209ee94eea26512ef2f
tyler <tylernaej@gmail.com>
2022-08-04 19:20:30 -0400

    added get reviews of current user route

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -13,22 +13,54 @@ router.get('/current',
         const { user } = req;
 
         let response = {}
-        let responseArray = []
         
         const reviews = await Review.findAll({
             where: {
                 userId: user.dataValues.id
             },
-            include: [
-                {model: User, attributes: ['id','firstName','lastName']},
-                {model: Spot, attributes: ['id','ownerId','address','city','state','country','lat','lng','name','price']},
-                {model: Image, attributes: ['id','url']}
-            ],
-            group: ["Spot.id"],
+            group: ["Review.id"],
             raw: true
         })
-        
-        res.send(reviews)
+       
+        const reviewer = await User.findByPk(user.id, {
+            attributes: ['id','firstName','lastName'],
+            raw: true
+        })
+        reviews.forEach(review => {
+            console.log(review)
+            review['User'] = reviewer
+        });
+
+        const spots = await Spot.findAll({
+            attributes: ['id','ownerId','address','city','state','country','lat','lng','name','price'],
+            raw:true
+        })
+        reviews.forEach(review => {
+            for(let i = 0; i < spots.length; i++) {
+                if(review['spotId'] === spots[i].id){
+                    review['Spot'] = spots[i]
+                }
+            }
+        })
+
+        const images = await Image.findAll({
+            attributes: ['id','url','reviewId'],
+            raw: true
+        })
+        reviews.forEach(review => {
+            for (let i = 0; i < images.length; i++) {
+                if(review.id === images[i].reviewId){
+                    let imagesObject = {}
+                    imagesObject['id'] = images[i].id
+                    imagesObject['url'] = images[i].url
+                    imagesObject['imageableId'] = images[i].reviewId
+                    review['Images'] = imagesObject
+                }
+            }
+        })
+        response['Reviews'] = reviews
+
+        res.send(response)
     }
     )
 
