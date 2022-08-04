7b6bb54281cdb93666134767cc0bf5b93864a5ff
tyler <tylernaej@gmail.com>
2022-08-04 15:36:14 -0400

    added structure into api/bookings, images, and reviews

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,10 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const spotsRouter = require('./spots.js')
+const spotsRouter = require('./spots.js');
+const bookingsRouter = require('./bookings.js');
+const imagesRouter = require('./images.js');
+const reviewsRouter = require('./reviews.js')
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -13,6 +16,9 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
+router.use('/bookings', bookingsRouter);
+router.use('/images', imagesRouter);
+router.use('/reviews', reviewsRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
