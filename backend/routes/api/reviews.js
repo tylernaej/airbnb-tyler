103190d2df282fc46d6a57c744b9a57a16793322
tyler <tylernaej@gmail.com>
2022-08-04 23:52:05 -0400

    added add image to review route

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -6,6 +6,7 @@ const { requireAuth } = require('../../utils/auth')
 const {Booking, Image, Review, Spot, User, sequelize} = require('../../db/models');
 const { response } = require('express');
 
+
 router.get('/current',
     requireAuth,
     async (req, res) => {
@@ -64,6 +65,66 @@ router.get('/current',
         res.json(response)
     }
 );
+router.post('/:reviewId/images',
+    requireAuth,
+    async (req, res) => {
+        
+        const { user } = req;
 
+        const review = await Review.findByPk(req.params.reviewId)
+
+        const reviewImages = await Image.findAll({
+            where: {
+                reviewId: review.id
+            },
+            raw: true
+        })
+
+        if(reviewImages.length > 9) {
+            res.status(403);
+            res.json({
+                "message": "Maximum number of images for this resource was reached",
+                "statusCode": 403
+              })
+        }
+
+        if(review.userId !== user.id){
+            res.status(403);
+            res.json({
+                "message": "Forbidden",
+                "statusCode": 403
+              })
+        }
+
+        if(!review){
+            res.status(404)
+            res.json({
+                "message": "Review couldn't be found",
+                "statusCode": 404
+              })
+        }
+
+        const {
+            url,
+            previewImage
+        } = req.body;
+
+        const newImage = Image.build({
+            url,
+            previewImage,
+            spotId: review['spotId'],
+            reviewId: review.id,
+            userId: user.id
+        })
+        await newImage.save()
+
+        let response= {}
+        response['id'] = newImage.id
+        response['imageableId'] = review.id
+        response['url'] = newImage.url
+
+        res.send(response)
+    }
+)
 
 module.exports = router;
