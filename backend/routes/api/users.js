39ccb85cc3d1c4810249bd7626606bcec9eb4430
tyler <tylernaej@gmail.com>
2022-08-08 00:01:27 -0400

    added email checker

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,6 +4,7 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const e = require('express');
 const router = express.Router();
 
 //signUp validation
@@ -34,27 +35,34 @@ const validateSignup = [
     .withMessage('Password must be 6 characters or more.'),
   handleValidationErrors
 ];
+
+const emailChecker = (req) => {
+  const { email } = req.body.email
+  const users = await User.findAll({raw: true})
+
+  users.forEach(user => {
+    if(user.email === email) {
+      res.status(403)
+      res.json({
+        "message": "User already exists",
+        "statusCode": 403,
+        "errors": {
+          "email": "User with that email already exists"
+        }
+      })
+    }
+  });
+  
+}
 //Sign up
 router.post(
     '/',
     validateSignup,
+    emailChecker(req),
     async (req, res) => {
+
       const { firstName, lastName, email, password, username } = req.body;
 
-      const users = await User.findAll({raw: true})
-
-      users.forEach(user => {
-        if(user.email === email) {
-          res.status(403)
-          res.json({
-            "message": "User already exists",
-            "statusCode": 403,
-            "errors": {
-              "email": "User with that email already exists"
-            }
-          })
-        }
-      });
       const user = await User.signup({ firstName, lastName, email, username, password });
   
       await setTokenCookie(res, user);
