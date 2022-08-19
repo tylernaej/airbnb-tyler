de6fb40c5cdc55ebfa9546b873885e192c3c6f97
tyler <tylernaej@gmail.com>
2022-08-19 18:42:41 -0400

    added modal

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -2,7 +2,7 @@ import { csrfFetch } from './csrf';
 
 const SET_USER = 'session/setUser';
 const REMOVE_USER = 'session/removeUser';
-
+//Action Creators
 const setUser = (user) => {
   return {
     type: SET_USER,
@@ -15,7 +15,7 @@ const removeUser = () => {
     type: REMOVE_USER,
   };
 };
-
+//Thunk Action Creators
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
@@ -30,6 +30,39 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const restoreUser = () => async dispatch => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const signup = (user) => async (dispatch) => {
+  const { firstName, lastName, username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
+//Reducers
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
