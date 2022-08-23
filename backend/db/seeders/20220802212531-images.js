99ab5e234346a08f36f078c69af77091a3f3317a
tyler <tylernaej@gmail.com>
2022-08-23 19:04:03 -0400

    updated seeders

--- backend/db/seeders/20220802212531-images.js
+++ backend/db/seeders/20220802212531-images.js
@@ -5,26 +5,190 @@ module.exports = {
 
     await queryInterface.bulkInsert('Images', [
       {
-        url: 'https://i.insider.com/5f5b7fdc7ed0ee001e25ebec?width=1000&format=jpeg&auto=webp',
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-26511667/original/984819d8-ac2a-4010-bd6b-c88306604275.jpeg?im_w=1200',
         previewImage: true,
-        spotId: 3,
+        spotId: 1,
         reviewId: 1,
         userId: 2
       }, 
       {
-        url: "https://www.jonesaroundtheworld.com/wp-content/uploads/2020/12/Vacation-Rental-New-Hampshire.jpg",
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-26511667/original/a97b3190-976a-409f-84f7-92959dad3a85.jpeg?im_w=720',
         previewImage: false,
-        spotId: 4,
-        reviewId: 2,
-        userId: 1
+        spotId: 1,
+        reviewId: 1,
+        userId: 2
       },
       {
-        url: "https://www.jonesaroundtheworld.com/wp-content/uploads/2020/11/Luxury-New-Hampshire-Airbnb.jpg",
+        url: 'https://a0.muscache.com/im/pictures/cb593f9d-fc45-40a6-8bc4-254765dae8c6.jpg?im_w=720',
+        previewImage: false,
+        spotId: 1,
+        reviewId: 1,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-26574926/original/572ffe5c-e871-4957-981f-872c6c935918.jpeg?im_w=1200',
         previewImage: true,
         spotId: 2,
+        reviewId: 2,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-26574926/original/0bc64c7c-7343-4b47-b83f-99080107594c.jpeg?im_w=720',
+        previewImage: false,
+        spotId: 2,
+        reviewId: 2,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-26574926/original/5649f394-6bc3-4414-99ba-0e8a82ee8111.jpeg?im_w=1200',
+        previewImage: false,
+        spotId: 2,
+        reviewId: 2,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-26574926/original/49c72e04-97a5-4a62-a0b4-d5cbec6298d5.jpeg?im_w=720',
+        previewImage: false,
+        spotId: 2,
+        reviewId: 2,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-51937032/original/276c7cdd-d00d-4c30-b036-e201313a892a.jpeg?im_w=1200',
+        previewImage: true,
+        spotId: 3,
         reviewId: 3,
-        userId: 5
-      }
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/403f994e-3073-4042-938a-4128a12a51b1.jpg?im_w=720',
+        previewImage: false,
+        spotId: 3,
+        reviewId: 3,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/15cc9b28-ed50-422e-9502-bd1837b50e9e.jpg?im_w=720',
+        previewImage: false,
+        spotId: 3,
+        reviewId: 3,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/dbb96ff7-9145-4f3f-9a87-0c0994981d59.jpg?im_w=720',
+        previewImage: false,
+        spotId: 3,
+        reviewId: 3,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-668850861862940741/original/7091a16d-00c8-404f-a4cf-85fad49f7e82.jpeg?im_w=1200',
+        previewImage: true,
+        spotId: 4,
+        reviewId: 4,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-668850861862940741/original/1bc6f2de-0363-430c-a283-478d3b0e87f0.jpeg?im_w=720',
+        previewImage: false,
+        spotId: 4,
+        reviewId: 4,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/7e278dec-7878-46f3-a99a-e33286df041f.jpg?im_w=1200',
+        previewImage: true,
+        spotId: 5,
+        reviewId: 5,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-628449667521656812/original/ced4d744-2665-4e1a-8180-137462ced6a3.jpeg?im_w=720',
+        previewImage: false,
+        spotId: 5,
+        reviewId: 5,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-628449667521656812/original/0e4e35f9-6fd6-41a5-9e2f-915a65d1871a.jpeg?im_w=720',
+        previewImage: false,
+        spotId: 5,
+        reviewId: 5,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/8d46637b-7d3e-4425-b725-e0e65c209ce0.jpg?im_w=1200',
+        previewImage: true,
+        spotId: 6,
+        reviewId: 6,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/5b775a37-7390-43ef-9bd5-fad0e87bd4b5.jpg?im_w=720',
+        previewImage: false,
+        spotId: 6,
+        reviewId: 6,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-4546647/original/cda6739a-98ae-4a58-ac01-69cebcbbe7e5.jpeg?im_w=720',
+        previewImage: false,
+        spotId: 6,
+        reviewId: 6,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/b05457b3-182b-4465-acc5-b8e352220ab4.jpg?im_w=720',
+        previewImage: false,
+        spotId: 6,
+        reviewId: 6,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-623259436879508231/original/958600b8-2a2c-4f1e-a4b9-ceebb4554309.jpeg?im_w=1200',
+        previewImage: true,
+        spotId: 7,
+        reviewId: 7,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/miso/Hosting-623259436879508231/original/52fbf69d-8155-4a15-877c-3536bf51c2c3.jpeg?im_w=720',
+        previewImage: false,
+        spotId: 7,
+        reviewId: 7,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-660725475338628268/original/5670ab55-323f-4b9d-b80e-7ce57962ce8f.jpeg?im_w=1200',
+        previewImage: true,
+        spotId: 8,
+        reviewId: 8,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-660725475338628268/original/915cc27e-8fe3-416d-a77a-cdcc2318b1b2.jpeg?im_w=720',
+        previewImage: false,
+        spotId: 8,
+        reviewId: 8,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-660725475338628268/original/5127a821-0873-4838-951f-2b72ac581b0c.jpeg?im_w=720',
+        previewImage: false,
+        spotId: 8,
+        reviewId: 8,
+        userId: 2
+      },
+      {
+        url: 'https://a0.muscache.com/im/pictures/prohost-api/Hosting-660725475338628268/original/56bf1688-a2fd-485a-a105-4a095c37d60a.jpeg?im_w=720',
+        previewImage: false,
+        spotId: 8,
+        reviewId: 8,
+        userId: 2
+      },
+
+
+
 
     ], {});
   },
